5d843daceb8e42c3ee73af85ce88e1d138d47537
Jason Xue <jasonxue1228@gmail.com>
2019-08-14 20:21:10 -0600

    Done with list and detial view

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
+    description = models.CharField(max_length=120)
     content = models.TextField()
 
     def __str__(self):
--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
-import 'antd/dist/antd.css';
+import React from "react";
+import "antd/dist/antd.css";
+import CustomLayout from "./container/Layout";
+import BaseRouter from "./routes";
+import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-
     return (
       <div className="App">
+        <Router>
+          <CustomLayout>
+            <BaseRouter />
+          </CustomLayout>
+        </Router>
       </div>
     );
   }
