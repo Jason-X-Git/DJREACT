6f2a9d3103576ba199ccfac8f6b0c67bb771ce75
Jason Xue <jasonxue1228@gmail.com>
2019-08-25 10:13:46 -0600

    Added auth for frontend

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'api-auth/', include('rest_framework.urls')),
+    path(r'rest-auth/', include('rest_auth.urls')),
     path('api/', include('articles.api.urls'))
 ]
